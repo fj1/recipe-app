b9eb2346400ceffa44692b4fe8f3725e721974c7
Frances Jurek <fjurek@pros.com>
2017-09-15 12:52:11 -0500

    Add redux

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux';
+import recipesReducer from './recipesReducer';
+
+const rootReducer = combineReducers({recipes: recipesReducer});
+
+export default rootReducer;
