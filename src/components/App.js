f9cbda5663508ab5d100fe24131958933bb8d895
Frances Jurek <fjurek@pros.com>
2017-09-15 12:36:28 -0500

    Add json server to serve recipe data

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, {PureComponent} from 'react';
+import RecipesList from './RecipesList';
+
+class App extends PureComponent {
+  render() {
+    return <RecipesList/>;
+  }
+}
+
+export default App;
---
+++ src/components/RecipesList.js
@@ -0,0 +1,24 @@
+import React, {PureComponent} from 'react';
+
+class RecipesList extends PureComponent {
+  state = {
+    recipes: []
+  }
+
+  componentDidMount = async() => {
+    const response = await fetch('http://localhost:5000/recipes', {
+      method: 'GET',
+      headers: {accept: 'application/json'},
+    });
+    const recipes = await response.json();
+    this.setState({
+      recipes
+    })
+  }
+
+  render() {
+    return <div>{this.state.recipes.map(recipe => <div key={recipe.id}>{recipe.name}</div>)}</div>;
+  }
+}
+
+export default RecipesList;
