92ec38a1503c49bd9402a0ad745981c6cc1436a8
Frances Jurek <fjurek@pros.com>
2017-09-26 12:21:27 -0500

    add react-apollo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import configureStore from 'store/configureStore'
 
+import configureStore from 'store/configureStore'
 import App from 'components/App'
 
 const store = configureStore()
